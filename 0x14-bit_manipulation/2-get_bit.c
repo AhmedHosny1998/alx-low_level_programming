b591f17f959f64021a8d5122934cdfbf90752581
AhmedHosny1998 <AhmedHosny1998.com>
2023-08-22 06:48:31 -0700

    aa

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+    if(index >= sizeof(n) *8)
+        return(-1);
+    return(n >> index & 1);
+}
