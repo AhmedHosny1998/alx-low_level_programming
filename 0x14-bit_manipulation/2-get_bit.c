748c09e3bfa2641dcea5bfccc521ecf0d8539611
AhmedHosny1998 <AhmedHosny1998.com>
2023-08-22 06:56:39 -0700

    bbbb

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,9 +4,9 @@
  *
  * Return: Always 0.
  */
-int get_bit(unsigned long int n, unsigned int index);
-{
-    if(index >= sizeof(n) *8)
-        return(-1);
-    return(n >> index & 1);
-}
+	int get_bit(unsigned long int n, unsigned int index)
+	{
+    		if(index >= sizeof(n) *8)
+        		return(-1);
+    		return(n >> index & 1);
+	}
