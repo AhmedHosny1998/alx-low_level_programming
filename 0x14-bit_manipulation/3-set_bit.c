0a6aff1a80353a3e3b0d6238ac9db1e6bbd08a8a
AhmedHosny1998 <AhmedHosny1998.com>
2023-08-23 08:38:07 -0700

    tt

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,12 +7,13 @@
  *
  * Return: 1 for success, -1 for failure
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
-    	if(index >= sizeof(n) *8)
-        	return(-1);
-    	return(!!(*n |n=1L << index));
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
 }
 
 
