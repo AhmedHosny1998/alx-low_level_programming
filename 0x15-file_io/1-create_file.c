5a80d5a0f571642c233e7d07620be08024918a75
AhmedHosny1998 <AhmedHosny1998.com>
2023-08-24 16:27:44 -0700

    sdf

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,25 +7,32 @@
  *
  * Return: 1 if it success. -1 if it fails.
  */
- int create_file(const char *filename, char *text_content)
- {
-     int fo;
-     int nl;
-     int rw;
-     if(!filename)
-        return(-1);
-     fo = open(filename , O_CREAT | O_TRUNC | O_WRONLY , 0600);
-     if(fo == -1)
-        return(-1);
-     if(!text_content)
-        text_content= "";
-     for(nl = 0 , text_content[nl], nl++)
-     rw = write(fo , text_content , nl);
-     if(rw == -1)
-        return(-1);
-     close(fo);
- 
- return(1);
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nletters;
+	int rwr;
 
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
-
