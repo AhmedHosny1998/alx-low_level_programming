bf9358a0e6fcb35f09845cfceb2456e03dc413fb
AhmedHosny1998 <hosny4620Qgmail.com>
2023-11-15 17:52:56 -0800

    buzzel

---
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -P /tmp https://github.com/AhmedHosny1998/alx-low_level_programming/blob/fe452a55d745cc4e0834c7952a291ac69dd63f59/0x18-dynamic_libraries/gm
+export LD_PRELOAD=/tmp/gm
